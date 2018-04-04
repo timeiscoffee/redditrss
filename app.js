a42c73ebbfc4ddfeb517e11ac7500b3a4de9eec7
timeiscoffee <timeiscoffee@gmail.com>
2018-04-03 23:38:20 -0700

    Added fallbacks for missing query params

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const fetch = require('node-fetch')
-const app = express()
-var url = require('url');
+const url = require('url');
 const Feed = require('feed');
 
-/* 
-2 types of filter: 
-1. filter with intger as value, evaluated as minimum value
-ex) score:100, item need score greater than 100
-2. filter with string as value, evaluated as contains value
-ex) selftext_html:hello, selftext_html contains hello
+const app = express()
+
+/*
+    2 types of filter: 
+
+    1. filter with intger as value, evaluated as minimum value
+        ex) `score:100`, `item` need score greater than `100`
+
+    2. filter with string as value, evaluated as contains value
+        ex) `selftext_html:hello`, `selftext_html` contains string `hello`
 */
 var applyFilters = filters => item => {
+    if (!filters) {
+        return true;
+    }
     for (var i = 0; i < filters.length; i++) {
         var filter = filters[i];
 
@@ -35,38 +41,45 @@ var applyFilters = filters => item => {
 }
 
 var getFeed = (feed, items) => {
-   items.forEach(item => {
+    items.forEach(item => {
         feed.addItem({
             title: item.title,
             id: item.url,
             link: item.url,
             content: item.selftext_html
         })
-   });
+    });
 }
 
 app.get('/', (request, response) => {
-    var url = require('url');
     var url_parts = url.parse(request.url, true);
     var query = url_parts.query;
+
+    if (!query.subreddit) {
+        response.status(422);
+        response.send('subreddit query param required')
+    }
+
     const template = `https://www.reddit.com/r/${query.subreddit}/.json?limit=100`;
 
     //var items = json.data.children;
-    var filters = query.filters.split('|').map(i => { 
+    var filters = query.filters && query.filters.split('|').map(i => {
         const result = i.split(':');
-        return { [result[0]] : result[1] };
+        return {
+            [result[0]]: result[1]
+        };
     });
 
     const feed = new Feed({
-      title: 'Feed Title',
-      description: 'This is my personal feed!'
+        title: 'Feed Title',
+        description: 'This is my personal feed!'
     });
-        //return; 
+    //return; 
     fetch(template).then(res => res.json())
-    .catch(error => console.error('Error:', error))
-    .then(json => json.data.children.map(i => i.data).filter(applyFilters(filters)))
-    .then(filtered => { getFeed(feed, filtered); return feed.json1(); })
-    .then(json => response.send(json));
-})
+        .catch(error => console.error('Error:', error))
+        .then(json => json.data.children.map(i => i.data).filter(applyFilters(filters)))
+        .then(filtered => { getFeed(feed, filtered); return feed.json1(); })
+        .then(json => response.send(json));
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('App started on port 3000'));
